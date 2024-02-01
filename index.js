ea67fc6633204c85d3083f2278598b8cb84c4ffc
Hanamaria23 <hanamaria2307@gmail.com>
2024-02-01 14:39:06 +0700

    PAWTB-19 <message>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From BFLP it 22" }');
+    res.send('{ "response": "Hello From BFLP it 22 test" }');
 });
 
 app.get('/will', function (req, res) {
